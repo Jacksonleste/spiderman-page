ba966dd7f63cc6308ccaa11de0c735742e491928
Jackson Leste <jacksongoncalves.leste@gmail.com>
2022-02-09 23:16:21 -0300

    add flip-card

--- script/script.js
+++ script/script.js
@@ -1,4 +1,8 @@
 const btnMobile = document.querySelector('#mobile-button')
+let cards = document.querySelector("#cards")
+let linkCards = document.querySelectorAll('.linkCard')
+
+
 
 function toggleMenu(event){
     if(event.type === 'touchstart') event.prevent.defalt
@@ -14,8 +18,29 @@ function toggleMenu(event){
     }
 }
 
+function flipCard(){
+    let front = document.querySelector(".card.front")
+    let back = document.querySelector(".back")
 
+    if(front.classList.contains("active")){
+        front.classList.remove("active")
+        back.classList.remove("active")
+    }else{
+        front.classList.add("active")
+        back.classList.add("active")
+    }
+
+}
+
+function dontFlip(){
+    flipCard()
+}
 /* ----------------------- */
+
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+linkCards.forEach((link)=>{
+    link.addEventListener('click', dontFlip)
+})
