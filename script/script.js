54d9cff4e28ef461104101c54faa24e4d1a7bfa7
Jackson Leste <jacksongoncalves.leste@gmail.com>
2022-03-04 00:50:41 -0300

    add Slide funcionalities

--- script/script.js
+++ script/script.js
@@ -35,12 +35,75 @@ function flipCard(){
 function dontFlip(){
     flipCard()
 }
+
+const btnLeft = document.querySelector('#btnLeft'),
+      btnRight = document.querySelector('#btnRight')
+
+let carousel = document.querySelector('#carousel'),
+    slides = document.querySelectorAll('.sliderContent'),
+    width = slides[0].getBoundingClientRect().width,
+    count = slides.length,
+    slideAtual = 1
+
+function pass (){ carousel.style.transform = `translateX(${-width * slideAtual}px)`}
+
+pass()
+
+function nextSlide(){
+    if(event.keyCode == 39 || event.keyCode == undefined){
+        if(slideAtual == count-1) return
+        carousel.style.transition = 'transform ease-out .6s'
+        slideAtual ++
+        pass()
+    }
+}
+
+function prevSlide(){
+    if(event.keyCode == 37 || event.keyCode == undefined){
+        if(slideAtual == 0) return
+        carousel.style.transition = 'transform ease-out .6s'
+        slideAtual --
+        pass()
+    }
+}
+
+function lastSlides(){
+    if(slides[slideAtual].id == 'firstCopy'){
+        carousel.style.transition = 'transform 0s'
+        slideAtual = 1
+        pass()
+    }else if(slides[slideAtual].id == 'lastCopy'){
+        carousel.style.transition = 'transform 0s'
+        slideAtual = count-2
+        pass()
+    }
+}
+
+function resizeSlide(){
+    width = slides[0].getBoundingClientRect().width
+    pass()
+}
 /* ----------------------- */
 
 
 btnMobile.addEventListener('click', toggleMenu)
-btnMobile.addEventListener('touchstart', toggleMenu)
 
 linkCards.forEach((link)=>{
     link.addEventListener('click', dontFlip)
 })
+
+carousel.addEventListener('mouseenter', ()=>{
+    document.addEventListener('keydown', nextSlide)
+    document.addEventListener('keydown', prevSlide)
+})
+
+carousel.addEventListener('mouseleave', ()=>{
+    document.removeEventListener('keydown', nextSlide)
+    document.removeEventListener('keydown', prevSlide)
+})
+
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
+
+carousel.addEventListener('transitionend', lastSlides)
+window.addEventListener('resize', resizeSlide)
